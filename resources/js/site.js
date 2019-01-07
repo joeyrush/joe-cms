5c0397299fab1dfcbd33557eddd0840a51cd0356
Joe Macbook <joe_rushton@hotmail.co.uk>
2019-01-07 21:08:25 +0000

    Some small cleaning up

--- resources/js/site.js
+++ resources/js/site.js
@@ -5,20 +5,19 @@ $(document).ready(function() {
 	var myChart = new Chart(ctx, {
 		type: 'horizontalBar',
 		data: {
-			labels: ["Modern CSS", "PHP", "JS", "Laravel", "CakePHP", "Vue", "Git", "MySQL", "Utility-First CSS", "Linux" ],
+			labels: ["PHP", "Laravel", "CakePHP", "MySQL", "SQL Tuning", "Modern CSS", "JS", "Vue", "Git", "AWS/EC2/S3", "DevOps/Ansible", "Linux" ],
 			datasets: [{
 				label: 'Self-appointed Skill Level ',
-				data: [68, 85, 49, 73, 65, 60, 40, 55, 50, 25],
+				data: [80, 75, 70, 65, 60, 70, 49, 60, 45, 50, 19, 15, 22],
 				backgroundColor: [
-				'rgba(191, 53, 100, 0.85)',
-				'rgba(191, 53, 100, 0.8)',
-				'rgba(191, 53, 100, 0.75)',
-				'rgba(191, 53, 100, 0.7)',
-				'rgba(191, 53, 100, 0.7)',
-				'rgba(191, 53, 100, 0.65)',
-				'rgba(191, 53, 100, 0.45)',
-				'rgba(191, 53, 100, 0.45)',
-				'rgba(191, 53, 100, 0.4)'
+				'rgba(235,82,134, 0.7)',
+				'rgba(235,82,134, 0.7)',
+				'rgba(235,82,134, 0.7)',
+				'rgba(235,82,134, 0.7)',
+				'rgba(235,82,134, 0.7)',
+				'rgba(94,7,144, 0.7)',
+				'rgba(94,7,144, 0.7)',
+				'rgba(94,7,144, 0.7)',
 				],
 				borderColor: [
 				'rgba(255,99,132,1)',
@@ -32,6 +31,18 @@ $(document).ready(function() {
 			}]
 		},
 		options: {
+			tooltips: {
+				callbacks: {
+					label: function(tooltipItems, data) { 
+						if (typeof tooltipItems.yLabel == 'undefined') return false;
+
+						switch (tooltipItems.yLabel) {
+							default:
+								return ' ' + tooltipItems.yLabel + ': ' + tooltipItems.xLabel + ' skill-points (100 = master)';
+						}
+                    }
+				}
+			},
 			scales: {
 				xAxes: [{
 					display: true,
@@ -44,13 +55,13 @@ $(document).ready(function() {
 	                    stepSize: 25,
 	                    suggestedMax: 100,
 	                    fontColor: '#382b5f',
-	                    fontSize: 13,
+	                    fontSize: 14,
 	                    callback: function(label, index, labels) {
 					        switch (label) {
 					            case 0:
 					                return 'Brand New';
 					            case 25:
-					                return 'Getting It';
+					                return 'Confident';
 					            case 50:
 					                return 'Advanced';
 					            case 100:
--- src/Template/Element/loader.ctp
+++ src/Template/Element/loader.ctp
@@ -2,7 +2,7 @@
 <div id="loading">
 	<div id="loading-center">
 		<div id="loading-center-absolute">
-			<div class="text-xs-center"><h1>Loading the masterpiece...</h1></div>
+			<div class="text-xs-center"><h1 class="leading-loose tracking-wide font-light">Loading, please wait...</h1></div>
 			<div id="object"></div>
 		</div>
 	</div>
